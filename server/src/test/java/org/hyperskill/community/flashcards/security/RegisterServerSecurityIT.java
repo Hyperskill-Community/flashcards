8614dcc093cc215c0f3e2a904f41b71e0623f3c6
Viktor Martynov <2013alfabravo@gmail.com>
2024-01-04 02:07:09 +0400

    change mongo container setup for integration tests

--- server/src/test/java/org/hyperskill/community/flashcards/security/RegisterServerSecurityIT.java
+++ server/src/test/java/org/hyperskill/community/flashcards/security/RegisterServerSecurityIT.java
@@ -2,30 +2,25 @@ package org.hyperskill.community.flashcards.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hyperskill.community.flashcards.registration.UserDto;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.aot.DisabledInAotMode;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+
 @SpringBootTest
 @Testcontainers
 @AutoConfigureMockMvc
-@Disabled // must fix test container connections...
-@DisabledInAotMode // bug in Spring 3.2 @MockBean does not work in AOT mode (https://github.com/spring-projects/spring-boot/issues/36997)
 class RegisterServerSecurityIT {
 
     @Autowired
@@ -33,20 +28,15 @@ class RegisterServerSecurityIT {
 
     @Container
     @ServiceConnection
-    static MongoDBContainer mongoDbContainer =  new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+    static MongoDBContainer mongoDbContainer;
+
+    static {
+        mongoDbContainer = new MongoDBContainer("mongo:latest");
+        mongoDbContainer.setPortBindings(List.of("27017:27017"));
+    }
 
     @Autowired
     ObjectMapper objectMapper;
-
-    @BeforeAll
-    static void setup() {
-        mongoDbContainer.start();
-    }
-
-    @AfterAll
-    static void tearDown() {
-        mongoDbContainer.stop();
-    }
 
     @Test
     void registerUnauthenticatedValidJson_AddsUser() throws Exception {
