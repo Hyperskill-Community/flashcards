0ccce37005d79b514ca21a95b7e1f83dab149e78
Viktor Martynov <2013alfabravo@gmail.com>
2024-01-06 17:51:22 +0400

    Mongo container setup for integration tests (#1)
    * change mongo container setup for integration tests
    
    * move container setup to a config class
    
    * restore TestFlashcardsApplication.java

--- server/src/test/java/org/hyperskill/community/flashcards/TestFlashcardsApplication.java
+++ server/src/test/java/org/hyperskill/community/flashcards/TestFlashcardsApplication.java
@@ -1,23 +1,13 @@
 package org.hyperskill.community.flashcards;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration(proxyBeanMethods = false)
 public class TestFlashcardsApplication {
 
-    @Bean
-    @ServiceConnection
-    MongoDBContainer mongoDbContainer() {
-        return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
-    }
-
     public static void main(String[] args) {
-        SpringApplication.from(FlashcardsApplication::main).with(TestFlashcardsApplication.class).run(args);
+        SpringApplication
+                .from(FlashcardsApplication::main)
+                .with(TestFlashcardsApplication.class)
+                .run(args);
     }
-
 }
