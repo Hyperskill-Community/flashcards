9822931fa6c75e13e3bc5cb3c11b8679f1b272e5
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2024-01-08 13:06:39 +0100

    fix root build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
-    application
     id("org.springframework.boot") version libs.versions.spring.boot
     id("io.spring.dependency-management") version libs.versions.spring.dependency.management
+}
+
+repositories {
+    mavenCentral()
 }
 
 tasks.withType<BootRun> {
@@ -12,7 +15,7 @@ tasks.withType<BootRun> {
 
 tasks.register<BootRun>("bootRunFlashcards") {
     group = "build"
-    description = "Run föashcards app and start containers for mongodb and auth server"
+    description = "Run flashcards app and start containers for mongodb and auth server"
     dependsOn(":flashcards-server:bootRun")
 }
 
