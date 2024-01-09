ce7e4f7422fd167dc05e3843af0a954bdbb9c8b4
Jürgen Wißkirchen <juergen.wisskirchen@cofinpro.de>
2024-01-09 22:36:29 +0100

    fix to run testcontainer tests on fixed port by container reuse. (#32)
    * fix to run testcontainer tests on fixed port by container reuse.
    
    * configure testcontainer reuse for CI

--- server/src/test/java/org/hyperskill/community/flashcards/TestMongoConfiguration.java
+++ server/src/test/java/org/hyperskill/community/flashcards/TestMongoConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.TestcontainersConfiguration;
 
 import java.util.List;
 
@@ -13,7 +14,8 @@ public class TestMongoConfiguration {
     @Bean
     @ServiceConnection
     public MongoDBContainer mongoDBContainer() {
-        var container = new MongoDBContainer("mongo:7.0.4-jammy");
+        TestcontainersConfiguration.getInstance().updateUserConfig("testcontainers.reuse.enable", "true");
+        var container = new MongoDBContainer("mongo:7.0.4-jammy").withReuse(true);
         container.setPortBindings(List.of("27017:27017"));
         return container;
     }
--- server/src/test/java/org/hyperskill/community/flashcards/integration/ExampleDataInitializerIT.java
+++ server/src/test/java/org/hyperskill/community/flashcards/integration/ExampleDataInitializerIT.java
@@ -1,7 +1,7 @@
 package org.hyperskill.community.flashcards.integration;
 
 import org.hyperskill.community.flashcards.TestMongoConfiguration;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +11,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Disabled
+//@Disabled
 @SpringBootTest(classes = TestMongoConfiguration.class)
 class ExampleDataInitializerIT {
 
@@ -21,6 +21,11 @@ class ExampleDataInitializerIT {
 
     @Autowired
     MongoTemplate mongoTemplate;
+
+    @AfterEach
+    void setup() {
+        mongoTemplate.getDb().drop();
+    }
 
     @Test
     void whenNewContainerStarted_insertSampleData() {
