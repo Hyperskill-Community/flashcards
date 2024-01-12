0265889e1739d3f08b5e71f59a62d53dd9d909f6
Jürgen Wißkirchen <juergen.wisskirchen@cofinpro.de>
2024-01-12 07:24:00 +0100

    Poc vue connect (#37)
    * WIP: add useCardsService, provide new route for test add register form from vue to access app wth login data
    
    * fix Cors issue
    
    * access authenticated endpoint from Vue

--- server/src/main/java/org/hyperskill/community/flashcards/category/CategoryController.java
+++ server/src/main/java/org/hyperskill/community/flashcards/category/CategoryController.java
@@ -3,9 +3,10 @@ package org.hyperskill.community.flashcards.category;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.hyperskill.community.flashcards.category.mapper.CategoryMapper;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api/categories")
 @RequiredArgsConstructor
+@Slf4j
 public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
     public CategoryPageResponse getAllCategories(
             @Valid @Min(0) @RequestParam(name = "page", required = false, defaultValue = "0") int page,
-            @AuthenticationPrincipal Jwt jwt
-            ) {
-        var username = jwt.getSubject();
+            @AuthenticationPrincipal OidcUser oidcUser) {
+
+        var username = oidcUser.getSubject();
         var categories = categoryService.getCategories(username, page);
         var mapper = new CategoryMapper(username);
         return mapper.categoryPageToCategoryPageResponse(categories);
