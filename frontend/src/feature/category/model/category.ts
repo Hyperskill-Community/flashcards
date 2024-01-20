ec42e68122edeb936623c7ded9d08a59cf65527e
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2024-01-20 19:36:00 +0100

    fix access rights of categories

--- frontend/src/feature/category/model/category.ts
+++ frontend/src/feature/category/model/category.ts
@@ -1,7 +1,16 @@
+export type Action = {
+  action: string,
+  uri: string
+}
+
 export type Category = {
   id: string,
   name: string,
-  access: string,
+  actions: Action[],
   numberOfCards?: number,
   description?: string,
+}
+
+export const getAccess = (category: Category): string => {
+  return category.actions.map(item => item.action).join(', ');
 }
