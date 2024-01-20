4c4904f2b876d1274482b214dcb8fab82aef38fc
Nguyen Huynh <nhuynh9497@gmail.com>
2024-01-20 14:27:21 -0500

    refactored detail page by creating a separate detail component.

---
+++ frontend/src/feature/cardmanagement/components/CardDetailsComponent.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <v-card class="pa-2 ma-2 mx-auto d-flex flex-column justify-space-between" max-width="500px" min-height="300px">
+    <v-card-title class="text-center">
+      Card title
+    </v-card-title>
+    <v-card-subtitle>
+      sport, math, programming
+    </v-card-subtitle>
+    <v-card-text>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+      Aenean commodo ligula eget dolor. Aenean massa.
+      Cum sociis natoque penatibus et
+    </v-card-text>
+    <v-container>
+      <v-card-actions>
+        <v-btn class="bg-blue-accent-1 font-weight-medium">Edit</v-btn>
+        <v-btn class="bg-blue-accent-1 font-weight-medium">Delete</v-btn>
+      </v-card-actions>
+    </v-container>
+  </v-card>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- frontend/src/feature/cards/pages/CardDetailsPage.vue
+++ frontend/src/feature/cards/pages/CardDetailsPage.vue
@@ -1,31 +1,14 @@
 <template>
   <v-container>
     <h2 class="text-center">Card Details Page for card with Id: {{ props.id }}</h2>
-    <v-card class="pa-2 ma-2 mx-auto d-flex flex-column justify-space-between" max-width="500px" min-height="300px"
-            color="#E0F7FA">
-      <v-card-title class="text-center text-sm-h3">
-        Card '{{ props.id }}'
-      </v-card-title>
-      <v-card-subtitle>
-        sport, math, programming
-      </v-card-subtitle>
-      <v-card-text>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-        Aenean commodo ligula eget dolor. Aenean massa.
-        Cum sociis natoque penatibus et
-      </v-card-text>
-      <v-container>
-        <v-card-actions>
-          <v-btn class="bg-blue-accent-1 font-weight-medium">Edit</v-btn>
-          <v-btn class="bg-blue-accent-1 font-weight-medium">Delete</v-btn>
-        </v-card-actions>
-      </v-container>
-    </v-card>
+    <CardDetailsComponent></CardDetailsComponent>
   </v-container>
 </template>
 
 
 <script setup lang="ts">
+import CardDetailsComponent from "@/feature/cardmanagement/components/CardDetailsComponent.vue";
+
 const props = defineProps({
   id: String
 })
