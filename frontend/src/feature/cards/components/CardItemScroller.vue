7ca9e6bd12f43736551c9d92d04ee8452b94d1f1
Krzysztof Karbownik <77581765+codeofcarbon@users.noreply.github.com>
2024-02-18 14:11:08 +0100

    minor templates refactor

--- frontend/src/feature/cards/components/CardItemScroller.vue
+++ frontend/src/feature/cards/components/CardItemScroller.vue
@@ -1,17 +1,16 @@
 <template>
-  <v-infinite-scroll :max-height="'60vh'"
-                     :items="items"
+  <v-infinite-scroll :items="items"
                      :onLoad="fetchCardsPage"
+                     :max-height="'60vh'"
                      empty-text="No more cards">
     <template v-for="(item, index) in items" :key="item">
       <div
-        :class="['pa-2', 'd-flex', 'justify-space-between', 'align-center', index % 2 === 0 ? 'bg-grey-lighten-2' : '']">
-        <v-avatar color="primary" size="large" class="mr-5">{{ index + 1 }}</v-avatar>
+        :class="['pa-2', 'd-flex', 'justify-space-between', 'align-center', {'bg-grey-lighten-2': index % 2 === 0}]">
+        <v-avatar v-text="`${index + 1}`" color="primary" size="large" class="mr-5"/>
         {{ item.title }}
-        <v-spacer></v-spacer>
-        <v-avatar color="primary" size="large" class="mr-10">{{ item.type.toUpperCase() }}</v-avatar>
-        <open-mdi-button tooltip-text="Open Card Details"
-                         :clickHandler="() => openCard(item.id)"/>
+        <v-spacer/>
+        <v-avatar v-text="item.type.toUpperCase()" color="primary" size="large" class="mr-10"/>
+        <open-mdi-button :clickHandler="() => openCard(item.id)" tooltip-text="Open Card Details"/>
       </div>
     </template>
   </v-infinite-scroll>
@@ -28,9 +27,20 @@ const props = defineProps<({
   categoryId: string,
   titleFilter: string,
 })>();
+
 const items = ref([] as CardItem[]);
 const router = useRouter();
 const pagePointer = ref({current: 0, isLast: false});
+
+watch(() => props.titleFilter, async (newVal, oldVal) => {
+  if (newVal !== oldVal) {
+    pagePointer.value = {current: 0, isLast: false}; // reset the page pointer
+    items.value = []; // clear the items array
+    await fetchCardsPage({done: () => console.log("Manual reload after filter change")});
+  }
+});
+
+const openCard = (id: string) => router.push(`/card/${props.categoryId}/${id}`);
 
 const fetchCardsPage = async ({done}: { done: Function }) => {
   if (pagePointer.value.isLast) {
@@ -47,17 +57,5 @@ const fetchCardsPage = async ({done}: { done: Function }) => {
     items.value.push(cardItem);
   }
   done('ok');
-}
-
-watch(() => props.titleFilter, async (newVal, oldVal) => {
-  if (newVal !== oldVal) {
-    pagePointer.value = {current: 0, isLast: false}; // reset the page pointer
-    items.value = []; // clear the items array
-    await fetchCardsPage({done: () => console.log("Manual reload after filter change")});
-  }
-});
-
-const openCard = (id: string) => {
-  router.push(`/card/${props.categoryId}/${id}`);
-}
+};
 </script>
--- frontend/src/shared/pages/Welcome.vue
+++ frontend/src/shared/pages/Welcome.vue
@@ -1,17 +1,12 @@
 <template>
   <v-container>
     <v-responsive class="align-center text-center fill-height">
-      <v-img height="300" src="@/assets/logo.svg" />
-
-      <div class="text-body-2 font-weight-bold mb-n1">Welcome to</div>
-
-      <h1 class="text-h2 font-weight-bold">Flashcards</h1>
-
-      <div class="py-14" />
+      <v-img height="300" src="@/assets/logo.svg"/>
+      <div v-text="'Welcome to'" class="text-body-2 font-weight-bold mb-n1"/>
+      <h1 v-text="'Flashcards'" class="text-h2 font-weight-bold"/>
+      <div class="py-14"/>
     </v-responsive>
   </v-container>
-
 </template>
 
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup/>
