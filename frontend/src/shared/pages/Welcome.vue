280b89ae2ca497663f27228bfcd65c4d1fe10f88
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2024-05-04 23:34:27 +0200

    add AI generated sound welcome

--- frontend/src/shared/pages/Welcome.vue
+++ frontend/src/shared/pages/Welcome.vue
@@ -1,6 +1,9 @@
 <template>
   <base-card-page>
     <v-img height="300" src="@/assets/logo.svg"/>
+    <audio autoplay ref="audioRef">
+      <source src="../../assets/Welcome_Serena.mp3" type="audio/mpeg">
+    </audio>
     <v-card-text>
       <v-row class="d-flex flex-column align-center pa-0">
         <div v-text="'Welcome to'" class="text-body-2 font-weight-bold"/>
@@ -11,4 +14,12 @@
 </template>
 <script setup lang="ts">
 import BaseCardPage from "@/shared/pages/BaseCardPage.vue";
+import {onMounted, ref} from "vue";
+
+const audioRef = ref({} as HTMLAudioElement);
+
+onMounted(() => {
+  audioRef.value.volume = 0.1; // 10% volume
+});
+
 </script>
